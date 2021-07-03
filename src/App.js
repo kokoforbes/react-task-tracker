cb5ee600628eab2350df2b311ff6ce8bb923ab49
onikoko abdullahi <Onikokoabdullahi@gmail.com>
2021-07-03 17:53:35 +0100

    created footer component and added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
+import Footer from "./components/Footer"
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -84,6 +85,7 @@ function App() {
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks}  onDelete={deleteTask} onToggle={toggleReminder} />):("No Tasks To Show")}
+      <Footer />
     </div>
   );
 }
