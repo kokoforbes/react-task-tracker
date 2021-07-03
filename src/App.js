027171cff59335bf4e3f99072904ff2a73889ecf
onikoko abdullahi <Onikokoabdullahi@gmail.com>
2021-07-03 16:03:15 +0100

    Made components more dynamic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
+
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -23,19 +27,27 @@ function App() {
     }
   ])
 
+  //ADD TASK
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //DELETE TASK
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // TOGGLE REMINDER
-  const toggleReminder = (id ) => {
-    console.log(id)
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task ))
   }
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks}  onDelete={deleteTask} onToggle={toggleReminder} />):("No Tasks To Show")}
     </div>
   );
